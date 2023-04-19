c028669194e3e1f319c0809c2cb0582722801cf9
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-19 18:34:58 +0530

    Add files via upload

---
+++ operaters.java
@@ -0,0 +1,35 @@
+public class operaters {
+    public static void main(String[] args) {
+        //assignment =,+=,-=.*=
+        // int a=1;
+        // int  b= a+=1;
+        // System.out.println(b);
+
+        //arithemetic operater +-*%/
+        // pre increment = change valure+ use value
+        // int a=1;
+        // int b= ++a;
+        // System.out.println(a+" ,"+b);
+        // post increment = use value+ change value
+        // int a=1;
+        // int b=a++;
+        // System.out.println(a+" "+b);
+
+        // logical operater && || !
+        // System.out.println((5>4)&& (1>0));
+        // System.out.println(!(5>5));
+
+        // relational operater 
+        // int a=1;
+        // int b=3;
+        // System.out.println(a==b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+
+        //ternary operater
+        // int val= (5>20)?1:0;
+        // System.out.println(val);
+
+
+    }
+}

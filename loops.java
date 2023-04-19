e14cd2b24babe066f34db2cb3a6625542c67c7d5
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-19 22:07:24 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,145 @@
+import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
+public class loops {
+    public static void main(String[] args) {
+        // // write a program to print hw 100 times
+
+        // int i=0;
+        // while(i<10){
+        //     System.out.println("hw"+i);
+        //     i++;
+        // }
+    //     int i=0;
+    //     int sum=0;
+    //     while(i<=10){
+    //        sum+=i;
+    //        i++;
+    //     }
+    //     System.out.println(sum);
+    // }
+
+        //FOR LOOP
+        // for(int i=0;i<4;i++){
+        //     System.out.println("*******");
+        // }
+
+        // print the reverse of a number 
+        // int n= 31032;
+        // while(n>0){
+        //     int ld= n%10;
+        //     System.out.print(ld);
+        //     n=n/10;
+        // }
+
+
+        // reverse the number 
+
+        // int rev= 0;
+        // int n=310302;
+        // while(n>0){
+        //     int ld= n%10;
+        //     rev= (rev*10)+ld;
+        //     n= n/10;
+        // }
+        // System.out.println(rev);
+
+
+        //DO WHILE LOOP
+        // int i=0;
+        // do{
+        //     System.out.println("hw");
+        //     i++;
+        // }while(i<=10);
+
+        //BREAK
+        // for(int i=0;i<10;i++){
+        //     if(i==4){
+        //         break;
+        //     }
+        //     System.err.print(i);
+        // }
+    //     do{
+    //     Scanner sc= new Scanner(System.in);
+    //     int n= sc.nextInt();
+    //     if(n%10==0){
+    //         break;
+    //     }
+    //     System.out.println(n +" ");
+
+    // }while(true);
+
+
+    //CONTINUE
+    // int i=0;
+    // for( ;i<10;i++){
+    //     if(i%2==0){
+    //        continue;
+    //     } System.err.println(i);
+    // }while(i<10);
+
+
+    //PRIME NUMBER
+
+    // Scanner sc= new Scanner(System.in);
+    // System.out.println("enter a number");
+    // int n= sc.nextInt();
+    // boolean isprime=true;
+    // if(n==2){
+    //     System.out.println("prime");
+    // }else{
+       
+    //     for(int i=2;i<=Math.sqrt(n);i++){
+    //         if(n%i==0){
+    //             isprime=false;
+               
+    //         }
+    //     }
+    
+    // }
+    // if(isprime==true){
+    //     System.out.println("prime");
+    // }else{
+    //     System.out.println("not Prime");
+    // }
+    
+
+    //factorial
+    // int n=5;
+    //     int fact=1;
+    //     for(int i=1;i<=n;i++){
+    //         fact*=i;
+
+    //     }
+    //     System.out.println(fact);
+
+    //multiplication table
+    // int n=10;
+    // for(int i=0;i<=n;i++){
+    //     System.out.println(n+"*"+i+"="+n*i);
+    // }
+
+    // for(int i=0;i<5;i++){
+    //     System.out.println("hello");
+    //     i+=2;
+    // }
+
+    // Scanner sc= new Scanner(System.in);
+    // int evensum=0;
+    // int oddsum=0;
+    // int choice;
+    // do{
+    //     int nbr=sc.nextInt();
+    //     if(nbr%2==0){
+    //         evensum+=nbr;
+    //     }else{
+    //         oddsum+=nbr;
+    //     }
+    //     System.out.println("enter 1 to continue or 0 to terminate");
+    //     choice= sc.nextInt();
+
+    // }while(choice==1);
+    // System.out.println(evensum+" "+oddsum);
+    }
+    
+}

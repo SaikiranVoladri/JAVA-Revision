81edccaaaba1e7276590ebeff1a6c72cd480b504
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-19 17:29:00 +0530

    uploaded basics.java file

---
+++ basics.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class basics{
+public static void main(String args[]){
+    // System.out.print("Hello world\n");
+    // System.out.println("Hellowor\rld");
+    // variables 
+
+    // int a = 10;
+    // int b=5;
+    // int sum= a+b;
+    // System.out.println(sum);
+
+    // int a= 5;
+    // int b=5;
+    // int product=a*b;
+    // System.out.println(product);
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("enter a ");
+    //  int a= sc.nextInt();
+    //  System.out.println("enter b");
+    //  int b= sc.nextInt();
+    //  int sum= a+b;
+    //  System.out.println(sum);
+
+
+
+    //Type Conversions;
+
+   // int a=10;
+   // float b=a; implicit conversion
+    // int c= b; float  cannot be converted to int lossty conversion takes
+    //int c= (int)b; Explicit conversion
+    //System.out.println(b+ ", "+c);
+
+
+    // Type conversion in expressions
+
+    // int a=10;
+    // float b=10.5f;
+    // double c= 10.1234;
+    // double sum= a+b+c;
+    // System.out.println(sum);
+
+    //ASSIGNMENT QUESTIONS
+// find the average from user input
+    // Scanner sc= new Scanner(System.in);
+    // int a=sc.nextInt();
+    // int b=sc.nextInt();
+    // int c=sc.nextInt();
+    // int avg= (a+b+c)/3;
+    // System.out.println("Average: "+avg);
+
+// find the area of Square
+    // Scanner Sc= new Scanner(System.in);
+    // int side= Sc.nextInt();
+    // int area= side * side;
+    // System.out.println(area);
+//total cost of three items with 18% gst
+//     float pen= 5.5f;
+//     float pencil=10.5f;
+//     float eraser= 10;
+
+//   double  total= (pen+pencil+eraser)*0.18;
+//   System.out.println(total);
+
+    // byte b=4;
+    // short s=512;
+    // char c='a';
+    // int i =1000;
+    // float f= 3.14f;
+    // double d= 99.9989;
+    // double result= (f*b)+(i%c)-(d*s);
+    // System.out.println(result);
+
+}
+}

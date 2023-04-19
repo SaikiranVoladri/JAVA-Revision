b6c10ba659f1e0c80e0c17dd7f26e893721f6de6
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-19 18:59:44 +0530

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,105 @@
+
+import java.util.*;
+public class conditionals {
+    public static void main(String[] args) {
+        //  if else condition
+        // int a=10;
+        // int b=11;
+        // if(a>b){
+        //     System.out.println("A");
+        // }else
+        //  System.out.println("B");
+
+        //if elseif condition
+        // int age=13;
+        // if(age>=18){
+        //     System.out.println("adult");
+        // }else if(age<18 && age>=13){
+        //     System.out.println("underated");
+        // }else{
+        //     System.out.println("kid");
+        // }
+
+        // odd or even
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("enter nbr");
+        // int n= sc.nextInt();
+        // // pass or Fail
+        // int res = n>=33?1:0;
+        // System.out.println(res);
+        // if(n%2==0)System.out.println("even");
+        // else System.err.println("odd");
+
+        // largest of 3
+        // int a=1;int b=0; int c=0;
+        // if(a>b && a>c){
+        //     System.out.println("a is greater");
+        // }
+        // else if(b>c){
+        //     System.out.println("b is greater");
+        // }else{
+        //     System.out.println("c ");
+        // }
+
+        // Scanner sc= new Scanner(System.in);
+        // int a= sc.nextInt();
+        // int b= sc.nextInt();
+        // char oper= sc.next().charAt(0);
+        // switch (oper){
+        //     case '+':
+        //     System.out.println(a+b);
+        //     break;
+        //     case '-':System.out.println(a-b);
+        //     break;
+        //     case '*': System.out.println(a*b);
+        //     break;
+
+        //     default:
+        //     System.out.println("invalid");
+        // }
+
+
+        //LEAP YEAR
+        // Scanner sc= new Scanner(System.in);
+        // int year= sc.nextInt();
+        // if(year%4==0){
+        //     if(year%100==0){
+        //         if(year%400==0){
+        //             System.out.println("leap year");
+        //         }else{
+        //             System.out.println(" not leap year");
+        //         }
+        //     }
+        //     else{   
+        //         System.out.println(" leap year ");
+        //     }
+        // }else{
+        //     System.out.println("not leap year");
+        // }
+
+        //WEEK DAY
+        // Scanner sc= new Scanner(System.in);
+        // int a= sc.nextInt();
+        
+        // switch (a){
+        //             case 1: System.out.println("monday");
+        //             break;
+        //             case 2: System.out.println("tuesday");
+        //             break;
+        //             case 3: System.out.println("wednesday");
+        //             break;
+        //             case 4: System.out.println("thursday");
+        //             break;
+        //             case 5: System.out.println("friday");
+        //             break;
+        //             case 6: System.out.println("saturday");
+        //             break;
+        //             default:
+        //             System.out.println("sunday");
+        // }
+
+
+
+    }
+    
+}

8560d06a2d74d6ba70525f3af0172fe8ccb036b5
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-20 08:30:48 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,40 @@
+public class pattern1 {
+     public static void main(String args[]){
+    //     for(int i=1;i<5;i++){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    // iNVERSTED STAR
+    // for(int i=4;i>=1;i--){
+    //     for(int j=i;j>=1;j--){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //HALF PYRAMID
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    //CHARACTER PATTERN 
+    // char ch='a';
+    // for(int i=0;i<=20;i++){
+    //     for(int j=0;j<=i;j++){
+    //         System.out.print(ch);
+    //         ch++;
+    //     }
+    //     System.out.println();
+    // }
+    
+}
+}

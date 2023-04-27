4ad9fe863f5ce7e573c05164896cb781047f9daf
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-04-27 23:11:01 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,74 @@
+// import java.util.*;
+// public class Arraylist {
+
+//     public static void main(String[] args) {
+//         ArrayList<ArrayList<Integer>> al= new ArrayList<>();
+//         ArrayList<Integer> l1= new ArrayList<>();
+//         ArrayList<Integer> l2= new ArrayList<>();
+//         ArrayList<Integer> l3= new ArrayList<>();
+
+//         for(int i=1;i<=10;i++){
+//             l1.add(i*2);
+//             l2.add(i*3);
+//             l3.add(i*5);
+//         }
+//         al.add(l1);
+//         al.add(l2);
+//         al.add(l3);
+
+//         for(int i=0;i<al.size();i++){
+//             ArrayList<Integer> curr= al.get(i);
+//             for(int j=0; j<curr.size();j++){
+//                 System.out.print(curr.get(j)+" ");
+//             }
+//             System.out.println();
+
+//         }
+       
+
+
+
+
+//         // al.add(1);
+//         // al.add(2);
+//         // al.add(3);
+//         // al.add(4);
+//         // al.add(5);
+//         // int i=1;
+//         // int j=3;
+       
+//         // int temp= al.get(i);
+//         // int temp2= al.get(j);
+//         // al.set(i,temp2);
+//         // al.set(j,temp);
+
+//         // System.out.println(al);
+        
+
+//         // int max=0;
+//         // for(int i=0;i<al.size();i++){
+//         //     if(max<al.get(i)){
+//         //         max= al.get(i);
+//         //     }
+//         // }
+//         // System.out.println(max);
+//         // System.out.println(al.size());
+//         // for(int i=0;i<al.size();i++){
+//         //     System.out.print(al.get(i)+" ");
+//         // }
+//         // System.out.println();
+//         // for(int i=al.size()-1;i>=0;i--){
+//         //     System.out.print(al.get(i)+" ");
+//         // }
+
+
+//         // System.out.println(al);
+//         // // System.out.println(al.get(0));
+//         // // System.out.println(al.remove(2));
+//         // // System.out.println(al);
+//         // System.out.println(al.contains(1));
+//         // al.add(0,10);
+//         // System.out.println(al);
+//     }
+    
+// }

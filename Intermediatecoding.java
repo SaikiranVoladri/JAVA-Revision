6df0cf8d82d6f8db79c966b2bf9debf07560137d
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-05-21 20:26:41 +0530

    Update Intermediatecoding.java

--- Intermediatecoding.java
+++ Intermediatecoding.java
@@ -4,11 +4,10 @@
      // // Amstrong number 153(1^3 +5^3+3^3)power = no of digit (153)3 , (1324)4
    // public static boolean Amstrong(int n){
 // //     //     int N=0;
-     //     int original=n;
-// //     //     int temp=n;
-//     //     while(n>0){
-    //         N++;        n=n/10;
-// //     //     }/     //     int sum=0;
+//     int original=n;
+     //     int temp=n;
+     //     while(n>0){
+   /         N++;        n=n/10;     //     }/     //     int sum=0;
      //     while(temp!=0){
  //     //         int ld= temp%10;
            
